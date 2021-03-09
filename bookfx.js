37e94041b61f7ac1ee8dea98a819a694f00b514e
anasne111 <ananse81@gmail.com>
2021-03-09 14:22:07 +0000

    changes made

--- bookfx.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('bn').addEventListener("click",function(){
-document.querySelector('.bg').style.display ='flex'   
-})
-
-document.querySelector('.close').addEventListener("click",function(){
-document.querySelector('.bg').style.display = 'none'
-})
