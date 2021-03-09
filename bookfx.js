1c983b823538d0da4faea39370dbd2f7c9ee0fd8
ananse111 <77746467+ananse111@users.noreply.github.com>
2021-03-09 13:35:20 +0000

    Add files via upload

---
+++ bookfx.js
@@ -0,0 +1,7 @@
+document.getElementById('bn').addEventListener("click",function(){
+document.querySelector('.bg').style.display ='flex'   
+})
+
+document.querySelector('.close').addEventListener("click",function(){
+document.querySelector('.bg').style.display = 'none'
+})
